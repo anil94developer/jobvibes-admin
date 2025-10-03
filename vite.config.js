77bce9d5172cfad4ceffa9ba18f6a3347d694f97
meghabhansali2911 <meghabhansali2529@gmail.com>
2025-10-03 23:23:16 +0530

    Updated : code

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // 👈 allows access from any host/IP
+    host: "0.0.0.0", // allow all IPs
+    port: 30002, // 👈 frontend runs on port 30002
+    allowedHosts: ["admin.jobvibes.in"], // allow your domain
+    proxy: {
+      "/api": {
+        target: "http://admin-api.jobvibes.in", // 👈 backend
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 30002,
+    allowedHosts: ["admin.jobvibes.in"],
   },
 });
